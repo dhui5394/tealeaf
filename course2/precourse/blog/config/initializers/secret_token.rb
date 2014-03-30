8e9544f434e2a6d857a4ae5c0314e25beb77ef5b
Dave Hui <davehui@gmail.com>
2014-03-30 13:36:47 -0700

    c1w2

---
+++ course2/precourse/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'd919c2315e0016ad46f78a6a227e63c5c0d9d1071157acd1f713a68b9581ffc5d51e0fc460195bbda3a87e8e001a645b0b03612892904eb45d48f868aa9722cc'
---
+++ course2/precourse/myapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = 'f268188b24256fb54325667ba22fa104843424fac0af06ad644d75f5b964661cf0b9690eb32eaf08d871065cf2decd86b7718238e142974ebc9d2859b73a011b'
